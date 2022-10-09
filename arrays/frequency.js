14fb93dfc91a066d8c94f601865054f21594b3b6
Blackwatch1910 <toretto_dominic1970@hotmail.com>
2022-10-09 22:37:02 +0530

    more array questions

---
+++ arrays/frequency.js
@@ -0,0 +1,28 @@
+const frequency = () => {
+
+    let arr = [1,2,8,3,2,2,2,5,1];
+
+    let visitedArray = [];
+
+    const visited = -1;
+
+    for (let i = 0; i < arr.length; i++) {
+        let count = 1;
+        for (let j = i + 1; j < arr.length; j++) {
+            if(arr[i] === arr[j]) {
+                count++;
+                visitedArray[j] = visited;
+            }
+        }
+
+        if (visitedArray[i] !== visited) {
+            visitedArray[i] = count;
+        }
+    }
+
+    visitedArray.map((item, index) => {
+        console.log(item);
+    })
+}
+
+frequency();
--- arrays/swap-alternate.cpp
+++ arrays/swap-alternate.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int swapAlternate(int arr[], int n) {
+    int t;
     for (int i = 0; i < n; i+=2) {
         if (i + 1 < n) {
-            swap(arr[i], arr[i + 1]);
+            t = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = t;
         }
     }
     
